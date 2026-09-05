836ebaafe7938ba659558ef50b901f87a1da3d76
shedrach igwe <igweshedrach@gmail.com>
2026-09-04 23:00:22 -0700

    project update

--- rlgshop/vite.config.js
+++ rlgshop/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import{resolve} from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build:{
+    rollupOptions:{
+      input:{
+        main:resolve(__dirname,'index.html'),
+        about:resolve(__dirname,'about.html'),
+        contact:resolve(__dirname,'contact.html'),
+        register:resolve(__dirname,'register.html'),
+        login:resolve(__dirname,'login.html'),
+        cart:resolve(__dirname,'cart.html'),
+        admin:resolve(__dirname,'admin.html'), 
+      }
+    }
+  }
 })
